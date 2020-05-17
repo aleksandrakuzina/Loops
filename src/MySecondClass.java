cda8ba2c1ebb6421af808067b5aa9f3cde0b025d
Kslum <alleksandra.kuzina@gmail.com>
2020-05-17 07:31:18 +0300

    обновлен

--- src/MySecondClass.java
+++ src/MySecondClass.java
@@ -4,17 +4,17 @@ public class MySecondClass {
 // todo Найти решене: выход из данной ошибки
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("ВВедите пункт 1 - сложение, 2 - вычитание, " +
                 "3- умножение. 4- деление   выход - 0");
-
-        int a = scanner.nextInt();
         int b = 9;
         int c = 3;
 
-        while (a != 0) {
 
-            if (a == 1) {
+        int a = scanner.nextInt();
+
+
+        while (a != 0) {
+               if (a == 1) {
                 System.out.println(b + c);
             } else if (a == 2) {
                 System.out.println(b - c);
@@ -22,9 +22,11 @@ public class MySecondClass {
                 System.out.println(b * c);
             } else if (a == 4) {
                 System.out.println(b / c);
+            }  else if (a == 0) {
+            break;
             } else {
                 System.out.println("ошибка");
-         kfjdggjdmflgknflf   }
+            };
         }
 
 
